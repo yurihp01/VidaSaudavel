8f088afbf388e70134ffc4945e8b19fbb4998bca
yurip <yurip@ciandt.com>
2018-02-19 10:54:27 -0300

    finalizando a tela principal

--- app/src/main/java/com/example/yurip/livehealth/view/MainActivity.java
+++ app/src/main/java/com/example/yurip/livehealth/view/MainActivity.java
@@ -14,4 +14,39 @@ public class MainActivity extends AppCompatActivity {
     void onClickedListDisease() {
         ListActivity_.intent(this).start();
     }
+
+    @Click(R.id.list_disease_text)
+    void onClickedListDiseaseText() {
+        ListActivity_.intent(this).start();
+    }
+
+    @Click(R.id.about_app)
+    void onClickedAboutApp() {
+        AboutActivity_.intent(this).start();
+    }
+
+    @Click(R.id.about_app_text)
+    void onClickedAboutAppText() {
+        AboutActivity_.intent(this).start();
+    }
+
+    @Click(R.id.nearest_hospital)
+    void OnClickedNearestHospital()  {
+        HospitalActivity_.intent(this).start();
+    }
+
+    @Click(R.id.nearest_hospital_text)
+    void OnClickedNearestHospitalText() {
+        HospitalActivity_.intent(this).start();
+    }
+
+    @Click(R.id.contact)
+    void onClickedContact() {
+        ContactActivity_.intent(this).start();
+    }
+
+    @Click(R.id.contact_text)
+    void onClickedContactText() {
+        ContactActivity_.intent(this).start();
+    }
 }
