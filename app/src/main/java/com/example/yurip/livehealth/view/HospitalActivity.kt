2fa21a82250ccbe8cf78b0a3205c4ae80babd927
yuri <yurip@ciandt.com>
2018-03-13 16:51:14 -0300

    implementando botoões de localização no mapa

--- app/src/main/java/com/example/yurip/livehealth/view/HospitalActivity.kt
+++ app/src/main/java/com/example/yurip/livehealth/view/HospitalActivity.kt
@@ -1,64 +1,208 @@
 package com.example.yurip.livehealth.view
 
 import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.FragmentActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
-
 import com.example.yurip.livehealth.R
+
 import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-
-import android.content.pm.PackageManager.PERMISSION_GRANTED
 import org.androidannotations.annotations.EActivity
 
-@EActivity(R.layout.activity_hospital)
-open class HospitalActivity : FragmentActivity(), OnMapReadyCallback, GoogleApiClient
-.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-    private var mMap: GoogleMap? = null
-    private var googleApiClient: GoogleApiClient? = null
 
-    public override fun onCreate(savedInstanceState: Bundle?) {
+class HospitalActivity : FragmentActivity(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+
+    private var mMap: GoogleMap? = null
+    internal var latitude: Double = 0.toDouble()
+    internal var longitude: Double = 0.toDouble()
+    private val PROXIMITY_RADIUS = 10000
+    internal var mGoogleApiClient: GoogleApiClient? = null
+    lateinit var mLastLocation: Location
+    internal var mCurrLocationMarker: Marker? = null
+    lateinit var mLocationRequest: LocationRequest
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        googleApiClient = GoogleApiClient.Builder(this)
-                .addApi(LocationServices.API)
+        setContentView(R.layout.activity_hospital)
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission()
+        }
+
+        //Check if Google Play Services Available or not
+        if (!CheckGooglePlayServices()) {
+            Log.d("onCreate", "Finishing test case since Google Play Services are not available")
+            finish()
+        } else {
+            Log.d("onCreate", "Google Play Services available.")
+        }
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun CheckGooglePlayServices(): Boolean {
+        val googleAPI = GoogleApiAvailability.getInstance()
+        val result = googleAPI.isGooglePlayServicesAvailable(this)
+        if (result != ConnectionResult.SUCCESS) {
+            if (googleAPI.isUserResolvableError(result)) {
+                googleAPI.getErrorDialog(this, result,
+                        0).show()
+            }
+            return false
+        }
+        return true
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap!!.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        //Initialize Google Play Services
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient()
+                mMap!!.isMyLocationEnabled = true
+            }
+        } else {
+            buildGoogleApiClient()
+            mMap!!.isMyLocationEnabled = true
+        }
+
+        val btnRestaurant = findViewById<Button>(R.id.btnDrugstore)
+        btnRestaurant.setOnClickListener(object : View.OnClickListener {
+            internal var Restaurant = "hospital"
+            override fun onClick(v: View) {
+                Log.d("onClick", "Button is Clicked")
+                mMap!!.clear()
+                val url = getUrl(latitude, longitude, Restaurant)
+                val DataTransfer = arrayOfNulls<Any>(2)
+                DataTransfer[0] = mMap
+                DataTransfer[1] = url
+                Log.d("onClick", url)
+                val getNearbyPlacesData = GetNearbyPlacesData()
+                getNearbyPlacesData.execute(*DataTransfer)
+                Toast.makeText(this@HospitalActivity, "Nearby Restaurants", Toast.LENGTH_LONG).show()
+            }
+        })
+
+        val btnHospital = findViewById<Button>(R.id.btnHospital)
+        btnHospital.setOnClickListener(object : View.OnClickListener {
+            internal var Hospital = "hospital"
+            override fun onClick(v: View) {
+                Log.d("onClick", "Button is Clicked")
+                mMap!!.clear()
+                val url = getUrl(latitude, longitude, Hospital)
+                val DataTransfer = arrayOfNulls<Any>(2)
+                DataTransfer[0] = mMap
+                DataTransfer[1] = url
+                Log.d("onClick", url)
+                val getNearbyPlacesData = GetNearbyPlacesData()
+                getNearbyPlacesData.execute(*DataTransfer)
+                Toast.makeText(this@HospitalActivity, "Nearby Hospitals", Toast.LENGTH_LONG).show()
+            }
+        })
+
+    }
+
+    protected fun buildGoogleApiClient() {
+        mGoogleApiClient = GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
                 .build()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        googleApiClient!!.connect()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        googleApiClient!!.disconnect()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-                                            grantResults: IntArray) {
-        if (requestCode == LOCATION_REQUEST_CODE) {
-            if (grantResults.size > 0 && grantResults[0] == PERMISSION_GRANTED) {
-                checkLocationAndAddToMap()
-            } else
-                Toast.makeText(this, "Location Permission Denied", Toast.LENGTH_SHORT).show()
-        }
+        mGoogleApiClient!!.connect()
     }
 
     override fun onConnected(bundle: Bundle?) {
-        prepareMap()
+        mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 1000
+        mLocationRequest.fastestInterval = 1000
+        mLocationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this)
+        }
+    }
+
+    private fun getUrl(latitude: Double, longitude: Double, nearbyPlace: String): String {
+
+        val googlePlacesUrl = StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?")
+        googlePlacesUrl.append("location=$latitude,$longitude")
+        googlePlacesUrl.append("&radius=$PROXIMITY_RADIUS")
+        googlePlacesUrl.append("&type=$nearbyPlace")
+        googlePlacesUrl.append("&sensor=true")
+        googlePlacesUrl.append("&key=" + getString(R.string.google_maps_key))
+        Log.d("getUrl", googlePlacesUrl.toString())
+        return googlePlacesUrl.toString()
     }
 
     override fun onConnectionSuspended(i: Int) {
+
+    }
+
+    override fun onLocationChanged(location: Location) {
+        Log.d("onLocationChanged", "entered")
+
+        mLastLocation = location
+        if (mCurrLocationMarker != null) {
+            mCurrLocationMarker!!.remove()
+        }
+
+        //Place current location marker
+        latitude = location.latitude
+        longitude = location.longitude
+        val latLng = LatLng(location.latitude, location.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(latLng)
+        markerOptions.title("Current Position")
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA))
+        mCurrLocationMarker = mMap!!.addMarker(markerOptions)
+
+        //move map camera
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+        mMap!!.animateCamera(CameraUpdateFactory.zoomTo(11f))
+        Toast.makeText(this@HospitalActivity, "Your Current Location", Toast.LENGTH_LONG).show()
+
+        //stop location updates
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this)
+            Log.d("onLocationChanged", "Removing Location Updates")
+        }
+        Log.d("onLocationChanged", "Exit")
 
     }
 
@@ -66,41 +210,67 @@ open class HospitalActivity : FragmentActivity(), OnMapReadyCallback, GoogleApiC
 
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        checkLocationAndAddToMap()
-    }
+    fun checkLocationPermission(): Boolean {
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-    private fun prepareMap() {
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-    }
+            // Asking user if explanation is needed
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)) {
 
-    private fun checkLocationAndAddToMap() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PERMISSION_GRANTED && ActivityCompat
-                        .checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission
-                    .ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_REQUEST_CODE)
-            return
-        }
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
 
-        val fusedLocationProviderApi = LocationServices.FusedLocationApi
-        val location = fusedLocationProviderApi.getLastLocation(googleApiClient)
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        MY_PERMISSIONS_REQUEST_LOCATION)
 
-        if (location == null) {
-            Toast.makeText(this, "O GPS não conseguiu localizar sua posição atual",
-                    Toast.LENGTH_SHORT).show()
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        MY_PERMISSIONS_REQUEST_LOCATION)
+            }
+            return false
         } else {
-            val markerOptions = MarkerOptions().position(LatLng(location.latitude, location.longitude)).title("Estou aqui!")
-
-            mMap!!.addMarker(markerOptions)
+            return true
         }
+    }
 
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted. Do the
+                    // contacts-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                                    Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                        if (mGoogleApiClient == null) {
+                            buildGoogleApiClient()
+                        }
+                        mMap!!.isMyLocationEnabled = true
+                    }
+
+                } else {
+
+                    // Permission denied, Disable the functionality that depends on this permission.
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+        }// other 'case' lines to check for other permissions this app might request.
+        // You can add here other case statements according to your requirement.
     }
 
     companion object {
-        private val LOCATION_REQUEST_CODE = 101
-    }
 
+        const val MY_PERMISSIONS_REQUEST_LOCATION = 99
+    }
 }
