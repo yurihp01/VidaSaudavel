dd27d644de099c05d680eeae3354834264a6e693
yurip <yurip@ciandt.com>
2018-04-12 11:09:38 -0300

    retomando

---
+++ app/src/main/java/com/example/yurip/livehealth/view/DownloadURL.kt
@@ -0,0 +1,51 @@
+package com.example.yurip.livehealth.view
+
+import android.util.Log
+
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+open class DownloadURL {
+
+    @Throws(IOException::class)
+    fun readUrl(strUrl: String): String {
+        var data = ""
+        var iStream: InputStream? = null
+        var urlConnection: HttpURLConnection? = null
+        try {
+            val url = URL(strUrl)
+
+            // Creating an http connection to communicate with url
+            urlConnection = url.openConnection() as HttpURLConnection
+
+            // Connecting to url
+            urlConnection.connect()
+
+            // Reading data from url
+            iStream = urlConnection.inputStream
+
+            val br = BufferedReader(InputStreamReader(iStream!!))
+
+            val sb = StringBuffer()
+
+            while (br.readLine() != null) {
+                sb.append(br.readLine())
+            }
+
+            data = sb.toString()
+            Log.d("downloadUrl", data)
+            br.close()
+
+        } catch (e: Exception) {
+            Log.d("Exception", e.toString())
+        } finally {
+            iStream!!.close()
+            urlConnection!!.disconnect()
+        }
+        return data
+    }
+}
