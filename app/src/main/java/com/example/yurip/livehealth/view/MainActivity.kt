1c1e1f4518af1e3a45969d25412a840ab7275b3f
yurip <yurip@ciandt.com>
2018-04-19 15:36:36 -0300

    botão de pesquisar no gps funcionando

--- app/src/main/java/com/example/yurip/livehealth/view/MainActivity.kt
+++ app/src/main/java/com/example/yurip/livehealth/view/MainActivity.kt
@@ -12,43 +12,42 @@ import org.androidannotations.annotations.EActivity
 open class MainActivity : AppCompatActivity() {
 
     @Click(R.id.list_disease)
-    internal fun onClickedListDisease() {
+    open fun onClickedListDisease() {
         ListActivity_.intent(this).start()
     }
 
     @Click(R.id.list_disease_text)
-    internal fun onClickedListDiseaseText() {
+    open fun onClickedListDiseaseText() {
         ListActivity_.intent(this).start()
     }
 
     @Click(R.id.about_app)
-    internal fun onClickedAboutApp() {
+    open fun onClickedAboutApp() {
         AboutActivity_.intent(this).start()
     }
 
     @Click(R.id.about_app_text)
-    internal fun onClickedAboutAppText() {
+    open fun onClickedAboutAppText() {
         AboutActivity_.intent(this).start()
     }
 
     @Click(R.id.nearest_hospital)
-    internal fun OnClickedNearestHospital() {
-        startActivity(Intent(this@MainActivity, HospitalActivity::class.java))
-
+    open fun OnClickedNearestHospital() {
+        startActivity(Intent(this, MapsActivity::class.java))
     }
 
     @Click(R.id.nearest_hospital_text)
-    internal fun OnClickedNearestHospitalText() {
-        startActivity(Intent(this@MainActivity, HospitalActivity::class.java))
+    open fun OnClickedNearestHospitalText() {
+        startActivity(Intent(this, MapsActivity::class.java))
     }
 
     @Click(R.id.contact)
-    internal fun onClickedContact() {
+    open fun onClickedContact() {
         ContactActivity_.intent(this).start()
     }
 
     @Click(R.id.contact_text)
-    internal fun onClickedContactText() {
+    open fun onClickedContactText() {
         ContactActivity_.intent(this).start()
     }
 }
